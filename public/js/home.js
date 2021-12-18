78bc6f022f8f5ff317d3d4d1076122c91322a59b
Eessh <76767406+Eessh@users.noreply.github.com>
2021-12-18 20:16:46 +0530

    Update home.js

--- public/js/home.js
+++ public/js/home.js
@@ -32,8 +32,6 @@ onAuthStateChanged(auth, async (user) => {
       // User is signed in, see docs for a list of available properties
       // https://firebase.google.com/docs/reference/js/firebase.User
       const uid = user.uid;
-      console.log("user: ", user);
-      console.log("email: ", user.email);
       currentUser = user;
     }
     else {
